5dd42ef52dbf7f97b93efbd35cebaffc216a4450
Kirin Patel <kirinpatel@gmail.com>
2025-01-01 22:10:34 -0500

    🔧 project cleanup (yarn -> npm, react-scripts -> vite, react 17 -> 19)

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-ReactDOM.render(
+const $app = document.createElement("div");
+const root = createRoot($app);
+
+document.body.appendChild($app);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
