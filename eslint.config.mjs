cde0e0705854b0028462e510f1b6a1711fe59a3c
Kirin Patel <kirinpatel@gmail.com>
2025-01-05 09:48:29 -0500

    chore: 🔧 update eslint to disable react/react-in-jsx-scope

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,9 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
